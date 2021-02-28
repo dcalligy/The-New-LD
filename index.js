a907bcacbc3421df6ba44f43643f51f34597ca81
dcalligy <dcalligy813@gmail.com>
2021-02-28 16:09:44 -0500

    First Blood

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const config = require("./config.json");
+const client = new Discord.Client();
+
+client.on("message", function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLocaleLowerCase();
+
+    if (command == "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`Pong! This message had a latency if ${timeTaken}ms`);
+    }
+});
+
+client.login(config.BOT_TOKEN);
