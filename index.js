327f5ea04bfc63a5bb0db051e27089063f203de8
dcalligy <dcalligy813@gmail.com>
2021-03-13 21:31:09 -0500

    Reeeeee

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
+'use strict';
 const Discord = require('discord.js');
-const config = require("./config.json");
+const { prefix, token } = require('./config.json');
+//const prefix = require('./config.json');
+//const emojis = require('./emojis.js');
+
 const client = new Discord.Client();
 
-client.on("message", function(message) {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+// we need to create an array of objects i think
+// also need to figure out how to get this to work in a json file?
+// maybe we create another json file? lyrics.json or something?
+const gucci_mane = [
+    "Overdose of sauce, no meat, just sauce",
+    "I pushed a lot of pills, a lot of peas, a lot of powder, It's Gucci Mane La Fleur and jiggalo ya cowards."
+];
 
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLocaleLowerCase();
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
-    if (command == "ping") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`Pong! This message had a latency if ${timeTaken}ms`);
+client.on('message', (msg) => {
+    // Talk back to ya boi
+    console.log(`${msg.channel.guild ? msg.channel.guild.name : 'DM'}`
+                + `# ${msg.channel.name} ${msg.author.tag}: ${msg.content}`);
+    if (msg.content.startsWith(`${prefix}ping`)) {
+        msg.channel.send('Pong!');
+    }
+    else if (msg.content.startsWith(`${prefix}ding`)) {
+        msg.channel.send('Dong!');
+    }
+    else if (msg.content.startsWith(`${prefix}uwu`)) {
+        msg.channel.send('OwO');
+    }
+    else if (msg.content.startsWith(`${prefix}owo`)) {
+        msg.channel.send('UwU');
+    }
+    else if (msg.content.startsWith(`${prefix}guwop`)) {
+        msg.channel.send('Dope fiend Willie used to finger fuck my rims!\n');
+        msg.channel.send('I wanna fuck tiny lil dirty ass.');
+    }
+    // This is sending an empty message for some reason...
+    else if (msg.content.startsWith(`${prefix}gucci`)) {
+        msg.channel.send([gucci_mane.content]);
+    }
+    else if (msg.content.startsWith(`${prefix}server`)) {
+        msg.channel.send(`This sever's name is: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);
     }
 });
 
-client.login(config.BOT_TOKEN);
+// This shit no work either...
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server.
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+});
+
+client.login(token);
