70115525e0d528ce61f0886b35ade5448a711dbe
dcalligy <dcalligy813@gmail.com>
2021-03-21 17:17:39 -0400

    Adding more shit 2021

---
+++ ping.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'ping',
+    description: 'Ping!',
+    execute(msg, args) {
+        msg.channel.send('Pong!');
+    }
+}
+// need one of these for:
+// ding
+// uwu
+// owo
+// guwop
+// gucci?
+// server
+// user-info
+// args-info
+// foo
+// kick
+// avatar
+// prune
