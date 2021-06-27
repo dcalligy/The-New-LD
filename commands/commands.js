c9b3bc8d53383aeb16352ee254f7261574ed0ebc
dcalligy <dcalligy813@gmail.com>
2021-06-26 22:52:36 -0400

    fix commands list command

--- commands/commands.js
+++ commands/commands.js
@@ -3,28 +3,27 @@ module.exports = {
   description: 'List of commands',
   execute(msg, args) {
     msg.channel.send(`
-      +avatar\m
-      +bigl\n
-      +ding\n
-      +droctagon\n
-      +40water\n
-      +gibbs\n,
-      +guwop\n,
-      +kanye\n,
-      +kick\n,
-      +nas\n,
-      +owo\n,
-      +uwu\n,
-      +ping\n,
-      +pat\n,
-      +prune\n,
-      +random\n,
-      +server\n,
-      +snoop\n,
-      +triple6\n,
-      +user-info\n,
-      +wutang\n
-
+      +avatar
+      +bigl
+      +ding
+      +droctagon
+      +40water
+      +gibbs
+      +guwop
+      +kanye
+      +kick
+      +nas
+      +owo
+      +uwu
+      +ping
+      +pat
+      +prune
+      +random
+      +server
+      +snoop
+      +triple6
+      +user-info
+      +wutang
     `)
   }
 }
