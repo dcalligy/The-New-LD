824243a0d95e03f5efd3eb28c211259adfa56cb6
dcalligy <dcalligy813@gmail.com>
2021-06-26 22:50:08 -0400

    Added commands list

--- commands/commands.js
+++ commands/commands.js
@@ -1,1 +1,30 @@
+module.exports = {
+  name: 'commands',
+  description: 'List of commands',
+  execute(msg, args) {
+    msg.channel.send(`
+      +avatar\m
+      +bigl\n
+      +ding\n
+      +droctagon\n
+      +40water\n
+      +gibbs\n,
+      +guwop\n,
+      +kanye\n,
+      +kick\n,
+      +nas\n,
+      +owo\n,
+      +uwu\n,
+      +ping\n,
+      +pat\n,
+      +prune\n,
+      +random\n,
+      +server\n,
+      +snoop\n,
+      +triple6\n,
+      +user-info\n,
+      +wutang\n
 
+    `)
+  }
+}
